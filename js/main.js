521d92dae463df0fc8302ed674ac518d8a0b748e
brm-moralesL <leonardo.morales@brm.com.co>
2019-03-12 11:09:27 -0500

    landing puentes estelar
    Agregados los terminos y condiciones

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+	console.log(window.innerWidth + " " + window.innerHeight);
+
 	$('.slider-hoteles').slick({
 		prevArrow: ('.nav-prev'),
 		nextArrow: ('.nav-next'),
